38765bd49c16b5bbdab49a32ac7c28316dd959ca
Denise Braithwaite <denisebraithw8@icloud.com>
2026-09-23 16:38:00 -0600

    Created CI Pipeline, initial tests, and configured jest, supertest, and lint

---
+++ src/tests/auth.test.js
@@ -0,0 +1,45 @@
+const request = require('supertest');
+const app = require('../service');
+// const { DB } = require('../database/database.js');
+// const { Role } = require('../model/model.js');
+
+function randomName() {
+  return Math.random().toString(36).substring(2, 12);
+}
+
+// async function createAdminUser() {
+//   let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
+//   user.name = randomName();
+//   user.email = user.name + '@admin.com';
+
+//   await DB.addUser(user);
+//   user.password = 'toomanysecrets';
+
+//   return user;
+// }
+
+const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
+let testUserAuthToken;
+
+beforeAll(async () => {
+  testUser.email = randomName() + '@test.com';
+  const registerRes = await request(app).post('/api/auth').send(testUser);
+  testUserAuthToken = registerRes.body.token;
+});
+
+test('login', async () => {
+  const loginRes = await request(app).put('/api/auth').send(testUser);
+  expect(loginRes.status).toBe(200);
+  expect(loginRes.body.token).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+
+  const user =  { ...testUser, roles: [{ role: 'diner' }] };
+  delete user.password;
+  expect(loginRes.body.user).toMatchObject(user);
+});
+
+test('get menu', async () => {
+  const menuRes = await request(app).get('/api/order/menu').set('Authorization', `Bearer ${testUserAuthToken}`);
+  expect(menuRes.status).toBe(200);
+  const crustyPizza = menuRes.body.find((item) => item.title === 'Crusty');
+  expect(crustyPizza).toBeDefined();
+});
